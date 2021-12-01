1e6f31c94b9eb5f86380bb76c86f3de37903ca6e
claudiama <claudiama0916@gmail.com>
2021-12-01 12:41:05 +0800

    change article layout

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"写在开博客之际",'url':"/2021/11/08/header-image.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"写在开博客之际",'url':"/2021/11/08/prelude.html"}]};
